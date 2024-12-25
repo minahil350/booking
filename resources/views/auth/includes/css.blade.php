7613366ea693d1c915bffc24a71a0f28d99b3dcb
minahil <zaibabasharat2717@gmail.com>
2024-12-25 18:41:22 +0500

    LaravelFresh

---
+++ resources/views/auth/includes/css.blade.php
@@ -0,0 +1,7 @@
+@vite([
+      
+      'resources/assets/vendors/mdi/css/materialdesignicons.min.css',
+      'resources/assets/vendors/css/vendor.bundle.base.css',
+      'resources/assets/css/style.css',
+                 
+      ])
